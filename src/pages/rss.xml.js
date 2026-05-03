02b85df84b5b3b3ad459039390f299985ec9e82d
andaugment <andaugment@users.noreply.github.com>
2026-05-03 16:38:09 +0900

    deploy: dtm 2026-05-03T07:38Z

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,10 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = (await getCollection('blog')).filter(p => !p.data.draft);
+	const now = Date.now();
+	const posts = (await getCollection('blog'))
+		.filter(p => !p.data.draft)
+		.filter(p => p.data.pubDate.valueOf() <= now);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
