f288345a74dd1333c6f814682504e212f37a11fc
andaugment <andaugment@users.noreply.github.com>
2026-05-08 20:37:57 +0900

    deploy: dtm 2026-05-08T11:37Z

--- src/content.config.ts
+++ src/content.config.ts
@@ -21,7 +21,6 @@ const blog = defineCollection({
 			saleExpiry: z.string().optional(),
 			xText: z.string().optional(),
 			evergreen: z.boolean().optional(),
-			allTimeLow: z.boolean().optional(),
 		}),
 });
 
