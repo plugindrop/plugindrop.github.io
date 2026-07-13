55ad5ffa2fe17d9fb5e14d0cd2feaa73d9301840
andaugment <andaugment@users.noreply.github.com>
2026-07-13 14:33:16 +0900

    deploy: dtm 2026-07-13T05:33Z

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = 'PluginDrop';
 export const SITE_DESCRIPTION = 'The best VST plugin deals, free downloads & sales — curated daily for music producers.';
 export const SITE_TWITTER = '@PluginDrop_biz';
+export const ENABLE_PIP_PREVIEW = false;
